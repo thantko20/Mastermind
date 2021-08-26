b593ef8ae9d8baed3ee622315ccda1462ce14de9
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-26 20:55:09 +0630

    Init play method

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 # Game of Mastermind
 
 # create a constant array of colors(R, G, B, W, B)
-KEYS = ['R', 'G', 'B', 'W', 'B'].freeze
+COLOR_KEYS = ['R', 'G', 'B', 'W', 'B'].freeze
+
+module Feedback
+  def key
+    
+  end
+end
 
 # Computer randomly selects secret colors
 # create computer class
@@ -9,10 +15,48 @@ KEYS = ['R', 'G', 'B', 'W', 'B'].freeze
 class Computer
   attr_reader :code
   def initialize
-    @code = KEYS.sample(4).join
+    @code = COLOR_KEYS.sample(4).join
   end
 end
 
-computer = Computer.new
-puts computer.code
 # Human Player has to guess the code
+class Player
+  attr_reader :guess_count, :guesses
+  def initialize
+    @guess_count = 0
+    @guesses = []
+  end
+end
+
+class Game
+  include Feedback
+
+  attr_reader :creator, :guesser
+  def initialize(creator, guesser)
+    @creator = creator
+    @guesser = guesser
+  end
+
+  def play
+    loop do
+      if @guesser.guess_count > 12
+        puts "Run out of guess"
+        break
+      end
+      puts @creator.code
+      puts "Enter your guess: "
+      guess = gets.chomp.to_s
+      if guess == @creator.code
+        puts 'Correct!'
+        break
+      end
+    end
+  end
+
+end
+
+computer = Computer.new
+player = Player.new
+game = Game.new(computer, player)
+
+game.play
